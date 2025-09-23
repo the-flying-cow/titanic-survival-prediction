0b485ccc2c54a9d8a4a7799749271268f21be927
the-flying-cow <anant07singh2004@gmail.com>
2025-09-23 20:20:00 +0530

    Use correct path values to load dataset in the correct variables.

--- src/Titanic_Survival_Prediction.py
+++ src/Titanic_Survival_Prediction.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# First we import all the necessary libraries that we will need.
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -22,27 +20,16 @@ TRAIN_PATH = os.path.join(BASE_DIR,"titanic_dataset", "train.csv")
 TEST_PATH  = os.path.join(BASE_DIR,"titanic_dataset", "test.csv")
 
 def main():
-    
-    # Upon extracting our zipfile we can see it contains- 
-    # test.csv, train.csv
-    # We will be training our model on the train dataset, and then test it against the test dataset.
 
-    train_data = pd.read_csv("titanic_dataset/train.csv")
-    test_data = pd.read_csv("titanic_dataset/test.csv")
+    train_data = pd.read_csv(TRAIN_PATH)
+    test_data = pd.read_csv(TEST_PATH)
 
     train_data
-
-
-    # ## EDA
-
     train_data.describe()
 
     sns.countplot(x="Survived",data= train_data)
     plt.title("Survival")
     plt.show()
-
-
-    # ## Boxplots
 
     num_cols= []
     num_cols= train_data.select_dtypes(include=['int64','float64']).columns
@@ -57,9 +44,6 @@ def main():
     plt.tight_layout()
     plt.show()
 
-
-    # ## Histograms
-
     num_cols= []
     num_cols= train_data.select_dtypes(include=['int64','float64']).columns
     plt.figure(figsize=(30,20))
@@ -73,29 +57,17 @@ def main():
     plt.tight_layout()
     plt.show()
 
-
-    # ## Preprocessing
-
     print(f"Null values:\n",train_data.isnull().sum())
 
     print(f"Duplicated data: ",train_data.duplicated().sum())
 
     print(f"Categorical Columns:\n",[col for col in train_data.columns if train_data[col].dtype=="object"])
 
-
-    # From the above observation, we can find that 'Age' and 'Cabin' column contain a lot of missing values. For the 'Embarked' column we can either drop them or fill them.
-
-    # Transforming the categorical data in our 'Sex' column to numerical data
-
-
     train_data['Sex'] = train_data['Sex'].astype(str).map({'male': 1, 'female': 0})
     train_data['Sex']
 
     test_data['Sex'] = test_data['Sex'].astype(str).map({'male': 1, 'female': 0})
     test_data['Sex']
-
-
-    # Handling the missing values in our 'Embarked' column and transforming the categorical data.
 
     print(train_data['Embarked'].mode())
     train_data['Embarked'].dtype
@@ -126,19 +98,11 @@ def main():
     train_data.head()
     test_data.head()
 
-
-    # ## Variables
-
-    # Dropping PassengerId, Name, Ticket, Cabin, Embarked column.
-    # These columns are either not significant for our model or contain a lot of NaN values.
-
     train_data.columns
-
 
     y = train_data["Survived"]
     x_with = train_data.drop(columns=["Survived", "PassengerId", "Name","Ticket","Cabin","Embarked"],axis=1)
 
-    #dropping Parch and Fare
     x_without = train_data.drop(columns=["Parch","Fare","Survived","PassengerId","Name","Ticket","Cabin","Embarked"],axis=1)
 
     test_passengerId = test_data["PassengerId"]
@@ -160,12 +124,6 @@ def main():
     result.summary()
     print(f"AIC after reducting our dataset {result.aic}")
 
-
-    # Though the difference between the AIC's before and after isn't much, dropping Parch and Fare columns from our feature tends to reduce our AIC for the model.
-
-    # Scaling down our Age and Fare columns
-
-
     X_train, X_test, y_train, y_test = train_test_split(x_without, y, test_size=0.2, random_state=7)
 
     scaler = StandardScaler()
@@ -175,21 +133,13 @@ def main():
     log_model = LogisticRegression(max_iter=1000)
     log_model.fit(X_train,y_train)
 
-
     preds =  log_model.predict(X_test)
-
 
     print("On the training dataset, following are the results:\n")
     print(f"Accuracy: {accuracy_score(y_test, preds)}")
     print("Confusion Matrix:\n",confusion_matrix(y_test, preds))
     print("Classification Report:\n",classification_report(y_test, preds))
 
-
-    test_data["log_Fare"]=test_data["log_Fare"].fillna(test_data["log_Fare"].median())
-    test_data= scaler.transform(test_data)
-    preds_on_test = log_model.predict(test_data)
-    submission = pd.DataFrame({"PassengerId": test_passengerId,"Survived": preds_on_test})
-    submission.to_csv("submission.csv", index=False)
 
 if __name__=="__main__":
     main()
