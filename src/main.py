831ab00a434736a83dc6ba66231ad4bd5c78a22c
the-flying-cow <anant07singh2004@gmail.com>
2025-10-09 18:43:52 +0530

    Implement a Random Forest Classifier and tune with hyper-parameter search

--- src/main.py
+++ src/main.py
@@ -2,10 +2,15 @@ from data_load import load_data
 from eda import eda_plot
 from pre_process import pre_processing
 from variables import variables
-from model import model_predict, final_report
-import os
+from model import model_predict, hyper_parameter_tune, final_report
 from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+import os
+from sklearn import set_config
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TRAIN_PATH = os.path.join(BASE_DIR,"titanic_dataset", "train.csv")
@@ -24,9 +29,20 @@ def main():
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
     
-    preds= model_predict(X_train, y_train, X_test)
     
-    final_report(y_test, preds)
+
+    log_pipe= Pipeline([("standard_scaling", StandardScaler()), ("classifier", LogisticRegression(max_iter= 1000))])
+    set_config(display= 'diagram')
+    print(log_pipe)
+    log_preds= model_predict(log_pipe,X_train, y_train, X_test)
+    final_report(y_test, log_preds)
+
+    rf_pipe= Pipeline([("standard_scaling", StandardScaler()), ("classifier", RandomForestClassifier(random_state= 7))])
+    rf_pipe= hyper_parameter_tune(rf_pipe, X_train, y_train)
+    set_config(display= 'diagram')
+    print(rf_pipe)
+    rf_preds= model_predict(rf_pipe,X_train, y_train, X_test)
+    final_report(y_test, rf_preds)
 
 if __name__=='__main__':
     main()
--- src/model.py
+++ src/model.py
@@ -1,12 +1,26 @@
-from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.model_selection import RandomizedSearchCV
+from scipy.stats import randint
 
-def model_predict(X_train, y_train, X_test):
-    log_model = LogisticRegression(max_iter=1000)
-    log_model.fit(X_train,y_train)
-    preds =  log_model.predict(X_test)
+def model_predict(pipe, X_train, y_train, X_test):
+    pipe.fit(X_train, y_train)
+    preds= pipe.predict(X_test)
+    
     return preds
 
+def hyper_parameter_tune(pipe, X_train, y_train ):
+    params= { "classifier__n_estimators": randint(100, 500),
+         "classifier__max_depth": randint(5, 20),
+         "classifier__min_samples_split": randint(2, 10),
+         "classifier__min_samples_leaf": randint(2, 9),
+         "classifier__max_features": ['sqrt', 'log2'],
+         "classifier__criterion":['gini', 'entropy']
+        }
+
+    random_search= RandomizedSearchCV(pipe, param_distributions= params, n_jobs= -1, cv= 5, n_iter= 100)
+    random_search.fit(X_train, y_train)
+    
+    return random_search.best_estimator_
 
 def final_report(y_test, preds):
 
